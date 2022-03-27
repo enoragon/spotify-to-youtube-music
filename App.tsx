c0f2d44d159c96fe912ec3addf627784a47a95d6
enoch <enoragon@gmail.com>
2022-03-27 12:39:20 -0600

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Text, StyleSheet } from "react-native";
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import * as Linking from 'expo-linking';
+
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
+  // const prefix = Linking.createURL('/');
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const linking = {
+    prefixes: [Linking.createURL('/'), 'https://app.example.com'],
+  };
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
+      
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      {/* content */}
+    </NavigationContainer>
         <StatusBar />
       </SafeAreaProvider>
     );
